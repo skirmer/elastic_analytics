8d2f50f8702fc3c8758020cbfaa92de8b6356a8c
Stephanie Kirmer <skirmer@journera.com>
2019-10-30 15:56:26 -0700

    Adding code samples for following along

---
+++ py_commands.py
@@ -0,0 +1,91 @@
+import json
+import uptasticsearch
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search
+from pandas.io.json import json_normalize
+
+# curl http://localhost:9200/utexas/_mapping > fields.json
+# cat fields.json
+
+query_dict = {"query": {"match_all": {}}}
+query_string = '{"query": {"match_all": {}}}'
+
+
+uptasticsearch.es_search(
+   es_host="http://localhost:9200",
+   query_body=query_string,
+   es_index="utexas"
+)
+
+
+es = Elasticsearch(['http://localhost:9200'])
+res = es.search(
+   index="utexas", 
+   body= query_dict
+)
+
+res['hits']['hits']
+json_normalize(res['hits']['hits'])
+
+
+res2 = Search(using = es).query("match", _index = 'utexas').execute()
+res2.to_dict()['hits']['hits']
+json_normalize(res2.to_dict()['hits']['hits'])
+
+
+# Additional Queries
+
+match_one = """{ 
+  "query": { "match": { "ciptitle.raw": "COMPUTER SCIENCE"} }
+}"""
+
+match_one_greater_one = """{  
+"query": 
+   { "bool" : { 
+      "must" : [ { "match": { "ciptitle.raw": "COMPUTER SCIENCE" } } 
+      		   , { "range" : { "cellcount" : { "gt" : 0 } } } ] 
+   } } 
+}"""
+
+match_two_greater_one = """{  
+"query": 
+   { "bool" : { 
+      "must" : [ { "match": { "ciptitle.raw": "COMPUTER SCIENCE" } }
+               , { "match": { "institution_id": "3599" } } 
+      		   , { "range" : { "cellcount" : { "gt" : 0 } } } ] 
+   } } 
+}"""
+
+query_and_filter = """{ "query":
+   { "bool": { 
+      "must": [ 
+         { "match": { "ciptitle.raw": "AREA STUDIES" } }
+         , { "match": { "deglevel": "Baccalaureate" } } 
+      ] , 
+      "filter": [ 
+         { "match": {"institution_id": "3599" } } 
+      ] 
+   } } 
+}"""
+
+query_and_sort = """{ "query": 
+	{ "bool" : { 
+		"must" : [ { "range": { "p50_earnings": { "gte" : 75000 } } }
+				 , { "match": { "institution_id": "3658" } }
+       		     , { "range": { "cellcount" : { "gt" : 0 } } } ] 
+	} 
+},
+"sort": {
+   "ciptitle.raw": "asc" 
+   }
+}"""
+
+summarize_one = """{
+	"aggs": {
+		"common_majors": {
+			"terms": {
+				"field": "ciptitle.raw" 
+			}
+		}
+	}
+}"""
---
+++ r_commands.R
@@ -0,0 +1,100 @@
+install.packages(c("uptasticsearch", "jsonlite","elastic"))
+
+uptasticsearch::get_fields(es_host = "http://localhost:9200", es_indices = "utexas")
+
+query_string <- '{"query": {"match_all":{}}}'
+
+test_up <- uptasticsearch::es_search(
+  es_host = "http://localhost:9200"
+  , es_index = "utexas"
+  , query_body = query_string
+  , size = 10)
+
+conn = elastic::connect(es_host = "http://localhost:9200")
+
+test_e <- elastic::Search(index = "utexas" 
+                          , body = query_string
+                          , size = 10
+                          , raw = TRUE
+                          , conn = conn)
+
+test_e2 <- jsonlite::fromJSON(test_e)$hits$hits
+
+test_e <- elastic::Search(
+  index = "utexas"
+  , q = "grad_cohort:*"
+  , size = 10
+  , conn = conn
+  , raw = TRUE)
+
+jsonlite::fromJSON(test_e)
+
+
+# Additional Queries
+
+match_one = '{ 
+  "query": { "match": { "ciptitle.raw": "COMPUTER SCIENCE"} }
+}'
+
+match_one_greater_one = '{  
+"query": 
+   { "bool" : { 
+      "must" : [ { "match": { "ciptitle.raw": "COMPUTER SCIENCE" } } 
+      		   , { "range" : { "cellcount" : { "gt" : 0 } } } ] 
+   } } 
+}'
+
+match_two_greater_one = '{  
+"query": 
+   { "bool" : { 
+      "must" : [ { "match": { "ciptitle.raw": "COMPUTER SCIENCE" } }
+               , { "match": { "institution_id": "3599" } } 
+      		   , { "range" : { "cellcount" : { "gt" : 0 } } } ] 
+   } } 
+}'
+
+query_and_filter = '{ "query":
+   { "bool": { 
+      "must": [ 
+         { "match": { "ciptitle.raw": "AREA STUDIES" } }
+         , { "match": { "deglevel": "Baccalaureate" } } 
+      ] , 
+      "filter": [ 
+         { "match": {"institution_id": "3599" } }
+      ] 
+   } } 
+}'
+
+query_and_sort = '{ "query": 
+	{ "bool" : { 
+		"must" : [ { "range": { "p50_earnings": { "gte" : 75000 } } }
+				 , { "match": { "institution_id": "3658" } }
+       		     , { "range": { "cellcount" : { "gt" : 0 } } } ] 
+	} 
+},
+"sort": {
+   "ciptitle.raw": "asc" 
+   }
+}'
+
+summarize_one = '{
+	"aggs": {
+		"common_majors": {
+			"terms": {
+				"field": "ciptitle.raw" 
+			}
+		}
+	}
+}'
+
+
+test_e <- elastic::Search(index = "utexas" 
+                          , body = match_one
+                          , size = 10
+                          , raw = TRUE
+                          , conn = conn)
+
+#### Reticulate ####
+library(reticulate)
+py_config()
+reticulate::use_python('/usr/local/bin/python3')
